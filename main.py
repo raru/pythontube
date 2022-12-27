6f9343e66cb6de1967f134d5442b680eb0b37659
raru <raru@mulisha.ec>
2022-12-27 16:10:51 -0300

    changed dowload location

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ def Download(link):
   youtubeObject = YouTube(link)
   youtubeObject = youtubeObject.streams.get_highest_resolution()
   try:
-    youtubeObject.download()
+    youtubeObject.download(output_path="/Users/raru/Public/Root/pythontube/pythontube/downloads")
   except:
     print("Deu ruim em baixar!")
   print("Deu bom em baixar! Uhuuuuuu")
 
 link = input("put yout ytube link here!! URL: ")
-Download(link)https://www.youtube.com/watch?v=EMlM6QTzJo0
+Download(link)
